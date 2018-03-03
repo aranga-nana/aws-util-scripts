6239b5fdd985f8829a107f794b3f808b19e91522
aranga-nana <aranga.nanayakkara@gmail.com>
2018-03-03 16:03:49 +1100

    create tag start time/end time

--- stopinator/lambda_function.py
+++ stopinator/lambda_function.py
@@ -12,7 +12,31 @@ import calendar
 
 #initalising
 default_timezone = "Australia/NSW"
+
+
 d={"1":"name"}
+def current_time(event):
+  current=[0,0,""]
+  tz = default_timezone
+
+  if not event:
+     tz = default_timezone
+     print "Loading default timezone`"
+  else:
+     tz = event.get("timezone")
+     if not tz:
+        tz = default_timezone
+        print "Loading default timezone "+default_timezone
+
+  print tz
+  timezone = pytz.timezone (tz)
+
+  datetime_with_tz = datetime.datetime.now(timezone)
+  current[0] = datetime_with_tz.hour
+  current[1] = datetime_with_tz.minute
+  current[2] = datetime_with_tz.strftime("%Y-%M-D %h:%m")
+  return current
+
 
 ## get tag value by key
 def get_tag_val(arg,tags):
@@ -42,13 +66,21 @@ def start_end_time(arg,tags):
 
 
 ## stop instance
-def stopInstance(ec2,iid):
+def stop_instance(ec2,instance,event):
+    iid = instance.get("InstanceId")
     #print('stopiing instance ',iid)
     print ec2.stop_instances(InstanceIds=[iid])
+    current = current_time(event)
+     
+    ec2.create_tag(Resources[iid],Tags=[{"Key":"stopinator:stop:time","Value":current[2]}]) 
 
-##start instance
-def startInstance(ec2,iid):
+##start instanc
+def start_instance(ec2,instance,event):
+    iid = instance.get("InstanceId")
     print ec2.start_instances(InstanceIds=[iid])
+
+    current = current_time(event)
+    ec2.create_tag(Resources=[iid], Tags=[{"Key":"stopinator:start:time","Value":current[2]}])
 
 ## load auto scaling load to a map
 def initaliseall():
@@ -102,26 +134,6 @@ def can_stop(ch,cm,time_b,time_e):
 
   return can   
 
-def currentTime(event):
-  current=[0,0]
-  tz = default_timezone
-
-  if not event:
-     tz = default_timezone
-     print "Loading default timezone`"
-  else:
-     tz = event.get("timezone")
-     if not tz:
-        tz = default_timezone
-        print "Loading default timezone "+default_timezone
-  
-  print tz 
-  timezone = pytz.timezone (tz)    
-    
-  datetime_with_tz = datetime.datetime.now(timezone) 	 
-  current[0] = datetime_with_tz.hour
-  current[1] = datetime_with_tz.minute
-  return current
 
 def instance_filter(event):
     df =["linear*","linear-dev*"]
@@ -144,7 +156,7 @@ def lambda_handler(event, context):
      
     initaliseall()
     #curent time calc (diffrent time zone support)
-    current = currentTime(event)
+    current = current_time(event)
     
     ch = current[0]
     cm = current[1]
@@ -183,15 +195,15 @@ def lambda_handler(event, context):
                   if iid in d:
                      suspendAsg(d[iid])
                      time.sleep(0.300)
-                  stopInstance(ec2,iid)
+                  stop_instance(ec2,i,event)
                   executeStop = True
             #start condition
             if not executeStop:
                if stateId == 80:
                   print "stateID",stateId 
                   if can_start(ch,cm,time_b,time_e):
-                     print "STARTING INSTANCE",iid," TIME ",datetime_with_tz
-                     startInstance(ec2,iid)
+                     print "STARTING INSTANCE:"+iid
+                     start_instance(ec2,i,event)
                              
     return "OK"
 
