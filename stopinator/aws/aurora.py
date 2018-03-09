e4d1e11a6f7ed58a63a1331da5c8a6542c1173a2
aranga-nana <aranga.nanayakkara@gmail.com>
2018-03-10 00:10:53 +1100

    create cluster from snapshot

--- stopinator/aws/aurora.py
+++ stopinator/aws/aurora.py
@@ -2,69 +2,136 @@ import time
 import boto3
 import utils
 
+CONST_TABLE="dev_aurora_db_meta"
 rds = boto3.client('rds',region_name="ap-southeast-2")
 dynamodb = boto3.client('dynamodb',region_name="ap-southeast-2")
 def get_table():
-    table =None
+
     try:
         table = dynamodb.create_table(
-            TableName='dev_aurora_metadata',
+            TableName=CONST_TABLE,
             KeySchema=[
                 {
                     'AttributeName': 'cluster_name',
                     'KeyType': 'HASH'
+                },
+                {
+                    'AttributeName': 'time:start',
+                    'KeyType': 'RANGE'
                 }
             ],
             AttributeDefinitions=[
                 {
-                    'AttributeName': 'TAGS',
-                    'AttributeType': 'SS'
-                },
-                {
-                    'AttributeName': 'start_time',
+                    'AttributeName': 'cluster_name',
                     'AttributeType': 'S'
-                },
-
+                }
             ],
             ProvisionedThroughput={
-                'ReadCapacityUnits': 5,
-                'WriteCapacityUnits': 5
+                'ReadCapacityUnits': 2,
+                'WriteCapacityUnits':2
             }
         )
 
     except Exception as e:
-        table =  dynamodb.Table('dev_aurora_metadata')
+        print e
         pass
 
-    return table
-def list_snapshots():
-    response = rds.describe_db_cluster_snapshots(
 
+def add_db_info(info):
+    dynamodb = boto3.resource('dynamodb',region_name="ap-southeast-2")
+    table = dynamodb.Table(CONST_TABLE)
+    response = table.put_item(
+            Item={
+            "cluster_name" :"bauuat1-db",
+            "time:start":"9:39",
+            "tags":{
+                "Name":"bauuat-test",
+                "Environment":"uat",
+                "owner":"aranga@linear.com.au"
+            },
+            "parameter_group":"bauuat1-db-rdsdbclusterparametergroup-19uta94ubs4k5",
+            "subnet_group":"bauuat-rdsstack-aurora-subnetgroup-a19289138",
+            "option_group":"default:aurora-5-6",
+            "security_group_name":"rds-launch-wizard",
+            "cluster_name":"bau-aurora1-cluster",
+            "db_instance_name":"bau-aurora1-instance1",
+            "time:start":"12:30"
+
+        }
+    )
+    print response
+
+
+def get_most_reason_snapshot(clusterIdentifier):
+    response = rds.describe_db_cluster_snapshots(
+        DBClusterIdentifier=clusterIdentifier,
         SnapshotType='manual'
 
     )
-    return response['DBClusterSnapshots']
+    slist = response['DBClusterSnapshots']
 
-def start_aurora(snapshot):
-    response = client.restore_db_cluster_from_snapshot(
+    slist = list(filter(lambda x: x['DBClusterSnapshotIdentifier'].startswith('start-db'), slist))
+    slist.sort(key=lambda k: k['SnapshotCreateTime'],reverse=True)
+    return slist[0]
 
-        DBClusterIdentifier='bauuat-linearbpms-cluster',
+def start_db(snapshot,cluster_name):
+    
+    response = rds.restore_db_cluster_from_snapshot(
+
+        DBClusterIdentifier=cluster_name,
         SnapshotIdentifier=snapshot,
-        Engine='string',
-        EngineVersion='string',
-        Port=123,
+        Engine='aurora',
         DBSubnetGroupName='bauuat-rdsstack-mysql-subnetgroup',
-        DatabaseName='linearbpms',
-        OptionGroupName='string',
         VpcSecurityGroupIds=[
-            'bauuat-RdsStack-SecurityGroup-12121',
+            'sg-b208d4cb',
         ],
         Tags=[
             {
                 'Key': 'stopinator:restore:status',
                 'Value': 'false'
             },
+            {
+                'Key': 'Name',
+                'Value': cluster_name
+            },
+            {
+                'Key': 'snaphost:name',
+                'Value': snapshot
+            }
         ]
+    )
+    print response
+    print "================ cluster creation =============================="
+    response = rds.create_db_instance(
+        DBInstanceIdentifier=cluster_name+"-instance",
+        DBInstanceClass="db.t2.small",
+        Engine='aurora',
+        DBClusterIdentifier=cluster_name,
+        Tags=[
+            {
+                'Key': 'stopinator:restore:status',
+                'Value': 'false'
+            },
+            {
+                'Key': 'Name',
+                'Value': cluster_name
+            },
+            {
+                'Key': 'snaphost:name',
+                'Value': snapshot
+            }
+        ]
+
+
+
+    )
+    print response
+    print "================ cluster instance creation =============================="
+
+        DBSnapshotIdentifier='start-db-acnonline-prod-2018-03-09-11-03',
+        LicenseModel='string',
+        Engine='aurora'
+
     )
 
     print response
--- stopinator/rds_aurora_stopinator.py
+++ stopinator/rds_aurora_stopinator.py
@@ -1,8 +1,10 @@
 from aws import utils,aurora
 
-table = aurora.get_table()
-print table
-snapshots = aurora.list_snapshots()
-snapshots.sort(key=lambda k: k['SnapshotCreateTime'])
-for s in snapshots:
-    print s['DBClusterSnapshotIdentifier'],s['SnapshotCreateTime']
+#aurora.add_db_info({})
+
+#s =aurora.get_most_reason_snapshot('acnonline-prod-1')
+#print s
+aurora.start_db('start-db-acnonline-prod-2018-03-09-11-03','acnonline-prod')
+#aurora.create_instance()
+#for s in snapshots:
+#    print s['DBClusterSnapshotIdentifier'],s['SnapshotCreateTime']
