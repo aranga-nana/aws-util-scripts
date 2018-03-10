edcb45df8f7fdeef8b824d3f660b0b15fea32b0e
aranga-nana <aranga.nanayakkara@gmail.com>
2018-03-10 22:48:11 +1100

    code cleanup

--- stopinator/rds_aurora_stopinator.py
+++ stopinator/rds_aurora_stopinator.py
@@ -1,16 +1,5 @@
 from aws import utils,aurora
 
-#aurora.add_db_info({})
-
-#s =aurora.get_most_reason_snapshot('acnonline-prod-1')
-#print s
-#aurora.start_db('start-db-acnonline-prod-2018-03-09-11-03','acnonline-prod')
-#result = aurora.list_cluster()
-
-#print result[0]
-#for rr in result:
-#    print rr['Tags'],rr['AvailableUpdate']
-
 aurora.init_table()
 def lambda_handler(event, context):
 
